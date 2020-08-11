ad36940a34f269fc8d243c56867f330c787e0651
liliya794 <liliyabutrimova@gmail.com>
2020-08-11 20:36:10 +0300

    добавлено мобильное меню попап

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-  console.log("Клик");
-  document
-    .querySelector(".navbar-menu")
-    .classList.toggle("navbar-menu-visible");
+$(document).ready(function () {
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
 });
